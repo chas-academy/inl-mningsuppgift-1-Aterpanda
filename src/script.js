ca0d13a33c5e998ba6da764aa0c1ebebbc39dc87
Aterpanda <129835213+Aterpanda@users.noreply.github.com>
2026-05-13 13:02:04 +0200

    Klar?

--- src/script.js
+++ src/script.js
@@ -39,7 +39,7 @@ function showContacts(contactBook, groupToShow) { // Rör ej denna kod
    
     for (contact of contactBook.contacts) {
         if (contact.group === groupToshow) {
-            return `namn: ${contact.name}, Telefon: ${contact.phone}`;
+            return `Namn: ${contact.name}, Telefon: ${contact.phone}`;
         }
     }
     return "Inga kontakter hittades i den valda gruppen."; // här ska sträng med namn och tekefonnummer visas i form av string interpolation
